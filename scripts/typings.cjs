6dcd9710b607bec156dbf8d0a639183c3788fd84
Yeskiy <eqwer24@outlook.com>
2023-03-14 00:17:52 +0200

    feat: Added types into dist build

---
+++ scripts/typings.cjs
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+
+const buildDir = path.join(__dirname, "../dist");
+(() => {
+    fs.readdir(buildDir, (error, dirs) => {
+        if (error) {
+            throw error;
+        }
+        dirs.forEach((dir) => {
+            const packageJsonFile = path.join(buildDir, dir, "/package.json");
+            switch (dir) {
+                case "cjs":
+                    fs.readFile(packageJsonFile, "utf8", (err, data) => {
+                        if (err) {
+                            throw err;
+                        }
+                        const packageJson = JSON.parse(data);
+                        packageJson.type = "commonjs";
+                        fs.writeFileSync(
+                            packageJsonFile,
+                            JSON.stringify(packageJson, null, 2),
+                            "utf8",
+                        );
+                    });
+                    break;
+                case "esm":
+                    fs.readFile(packageJsonFile, "utf8", (err, data) => {
+                        if (err) {
+                            throw err;
+                        }
+                        const packageJson = JSON.parse(data);
+                        packageJson.type = "module";
+                        fs.writeFileSync(
+                            packageJsonFile,
+                            JSON.stringify(packageJson, null, 2),
+                            "utf8",
+                        );
+                    });
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
+})();
